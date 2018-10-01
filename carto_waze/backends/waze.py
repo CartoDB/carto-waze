54296fc295de3225e4e2f0df6343d0457b9ea2ae
Dani Carrion <daniel@cartodb.com>
2018-10-01 14:42:56 +0200

    pep8

--- carto_waze/backends/waze.py
+++ carto_waze/backends/waze.py
@@ -2,15 +2,14 @@ import requests
 import csv
 import re
 from shapely import geos
-from shapely.geometry import Point, LineString
 
-from .base import Backend, with_datasource, with_filter, ALERT_FIELDS, JAM_FIELDS
+from .base import Backend, ALERT_FIELDS, JAM_FIELDS
 
 SRID = 4326
 geos.WKBWriter.defaults['include_srid'] = True
 
-class Waze(Backend):
 
+class Waze(Backend):
     def __init__(self, *args, url):
         data = requests.get(url=url)
 
@@ -21,27 +20,27 @@ class Waze(Backend):
 
         super().__init__(*args)
 
-    def convert(self,name):
+    def convert(self, name):
         '''
         thanks https://stackoverflow.com/a/1176023/3647833
         '''
         s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
         return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
-    
+
     def get_data(self):
         raise NotImplementedError
 
     def get_the_geom(self):
         raise NotImplementedError
 
-    def get_field(self,field_name):
+    def get_field(self, field_name):
         if field_name in self.waze_field_names:
             return field_name
         elif self.convert(field_name) in self.waze_field_names:
             return self.convert(field_name)
         elif field_name in self.CUSTOM_FIELD:
             return self.CUSTOM_FIELD[field_name]
-    
+
     def get_values(self, descriptor):
         writer = csv.writer(descriptor)
         writer.writerow(self.carto_field_names)
@@ -60,13 +59,14 @@ class Waze(Backend):
                 else:
                     row = row + (None,)
             print(row)
-            
-            writer.writerow(self.build_row_with_geom(row,the_geom.wkb_hex))
+
+            writer.writerow(self.build_row_with_geom(row, the_geom.wkb_hex))
+
 
 class Alert(Waze):
     common_fields = ALERT_FIELDS
     CUSTOM_FIELD = {
-        'nThumbsUp' : 'thumbs_up'
+        'nThumbsUp': 'thumbs_up'
     }
 
     def get_data(self):
@@ -79,11 +79,10 @@ class Alert(Waze):
         return self.get_point(data['location'])
 
 
-
 class Jam(Waze):
     common_fields = JAM_FIELDS
     CUSTOM_FIELD = {
-        'speedKMH' : 'speed_kmh'
+        'speedKMH': 'speed_kmh'
     }
 
     def get_data(self):
@@ -94,5 +93,3 @@ class Jam(Waze):
 
     def get_the_geom(self, data):
         return self.get_line(data['line'])
-
-
