2d62d784f84a01cb326504acf7f620cbef6ac6ac
Dani Carrion <daniel@cartodb.com>
2018-10-10 19:34:47 +0200

    add backends package, bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 setup(name="cartowaze",
       author="Jorge Sanz <jsanz@carto.com>, Daniel Carrión <daniel@carto.com>",
       description="Connect Waze data sources with CARTO",
-      version="0.0.1",
+      version="0.0.2",
       license="MIT",
       url="https://github.com/CartoDB/carto-waze",
       install_requires=["carto>=1.3.0", "psycopg2>=2.7.5", "shapely>=1.6.4"],
-      packages=["cartowaze"])
+      packages=["cartowaze", "cartowaze.backends"])
